ff68a800ec493d4fe11c66d5c6f447f3163f2dbb
Jesse Anderson <75538757+jesse-anderson@users.noreply.github.com>
2025-01-08 14:20:43 -0600

    Update update_and_run.sh

--- update_and_run.sh
+++ update_and_run.sh
@@ -4,13 +4,13 @@
 cd /home/pi/Desktop/OP-Crime || { echo "Failed to navigate to /home/pi/Desktop/OP-Crime"; exit 1; }
 
 # Pull the latest changes from GitHub
-git pull origin main >> /home/pi/Desktop/OP-Crime/logs/update_repo.log 2>&1
+git pull origin main >> /home/pi/Desktop/logs/update_repo.log 2>&1
 
 # Activate the virtual environment
 source /home/pi/Desktop/venv/bin/activate
 
 # Run the Python scripts
-python  weekly_crime_report.py >> /home/pi/Desktop/logs/weekly_crime_report.log 2>&1
-python  OakPark_Crime_Reporting_Web.py >> /home/pi/logs/OakPark_Crime_Reporting_Web.log 2>&1
+python weekly_crime_report.py >> /home/pi/Desktop/logs/weekly_crime_report.log 2>&1
+python OakPark_Crime_Reporting_Web.py >> /home/pi/Desktop/logs/OakPark_Crime_Reporting_Web.log 2>&1
 
 deactivate
