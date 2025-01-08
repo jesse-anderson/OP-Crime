a7641ea5536a3a21120bcca0d0cb932c62c10f23
Jesse Anderson <75538757+jesse-anderson@users.noreply.github.com>
2025-01-08 13:43:44 -0600

    Update update_repo.sh

--- update_repo.sh
+++ update_repo.sh
@@ -3,17 +3,11 @@
 # Navigate to the repository
 cd /home/pi/Desktop/OP-Crime
 
-# Activate virtual environment
-source venv/bin/activate
-
 # Reset any local changes
 git reset --hard
 
 # Pull the latest changes
 git pull origin main
-
-# Deactivate virtual environment
-deactivate
 
 # Log the update and script execution
 echo "$(date): Repository updated and scripts executed successfully." >> ~/Desktop/OP-Crime/update.log
